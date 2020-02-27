86dbea6ff8c5fc01f19d2ff22f2c7396cd97a2d5
Justin Hochschild <justinmhoch@gmail.com>
2020-02-26 20:13:37 -0500

    refactored special names code: found special names with initial file read.

--- index.php
+++ index.php
@@ -7,16 +7,21 @@
   $fullNames = array();
   $lastNames = array();
   $firstNames = array();
+  $specialNames = array();
+  $n = 25;
   /* Regex to match only names in the list that contain ONLY:
     -capital or lower case letters
     -apostrophes */
   $regex = '/^([a-zA-Z\']+),\s([a-zA-Z]+)/';
 
   // ******************* Getting Info From Text File *******************
-  $file = fopen('yesware-test-data-v1-8.txt', 'r');
+  $file = fopen('names.txt', 'r');
   $nextName = fgets($file);
   while(!feof($file)) {
     if(preg_match($regex, $nextName, $matches)) {
+      if(!in_array($matches[1], $lastNames) && !in_array($matches[2], $firstNames)) {
+        $specialNames[] = $matches[0];
+      }
       $fullNames[] = $matches[0];
       $lastNames[] = $matches[1];
       $firstNames[] = $matches[2];
@@ -32,8 +37,8 @@
   $uniqueFirstNameCount = uniqueNameCount($firstNames);
   $mostCommonLastNames = mostCommon($lastNames);
   $mostCommonFirstNames = mostCommon($firstNames);
-  $specialUniqueNames = specialUniqueNames($fullNames, $lastNames, $firstNames, 25);
-  $modifiedNames = theNameMangler($specialUniqueNames);
+  $nSpecialNames = array_slice($specialNames, 0, $n);
+  $modifiedNames = theNameMangler($nSpecialNames);
 
   // ************** Printing *******************
 
@@ -47,23 +52,23 @@
 
   print('<h2><u>Most Common Names</u></h2>');
   print('<h3>The 10 most common last names and the number of times they occur.</h3>');
-  print('<ul>');
+  print('<ol>');
   foreach($mostCommonLastNames as $key => $value) {
     print('<li>' . $key . ', ' . $value . '</li>');
   }
-  print('</ul>');
+  print('</ol>');
 
   print('<h3>The 10 most common first names and the number of times they occur.</h3>');
-  print('<ul>');
+  print('<ol>');
   foreach($mostCommonFirstNames as $key => $value) {
     print('<li>' . $key . ', ' . $value . '</li>');
   }
-  print('</ul>');
+  print('</ol>');
 
   print('<h2><u>Special and Modified Names</u></h2>');
   print('<h3>The first N number of specially unique names (in this case N=25)</h3>');
   print('<ol>');
-  foreach($specialUniqueNames as $value) {
+  foreach($nSpecialNames as $value) {
     print('<li>' . $value . '</li>');
   }
   print('</ol>');
--- names-functions.php
+++ names-functions.php
@@ -14,26 +14,10 @@ function mostCommon($nameArray) {
   return array_slice($array, 0, 10);
 }
 
-// Specially Unique
-/* Finds specially unique names by taking in fullname, lastname, and firstname arrays and the number of unique names you want to find. Creates test arrays to test against. loops through array using for statement and tests each last name and first name to see if it has been seen before via the test arrays. If it has not, the full name from the fullname array is added to the special array and returned. Each name looked at is added to the corresponding test array.*/
-function specialUniqueNames($fullNameArray, $lastNameArray, $firstNameArray, $number) {
-  $testlastName = array();
-  $testfirstName = array();
-  $special = array();
-  for($i=0; count($special)<$number; $i++) {
-    if(!in_array($lastNameArray[$i], $testlastName) && !in_array($firstNameArray[$i], $testfirstName)) {
-      $special[] = $fullNameArray[$i];
-    }
-    $testlastName[] = $lastNameArray[$i];
-    $testfirstName[] = $firstNameArray[$i];
-  }
-  return $special;
-}
-
 // Modified Names
 function theNameMangler($specialNames) {
   foreach($specialNames as $name) {
-    list($lastName, $firstName)=explode(', ', $name);
+    list($lastName[], $firstName[])=explode(', ', $name);
   }
   $firstName[]=array_shift($firstName);
   return array_combine($lastName, $firstName);
